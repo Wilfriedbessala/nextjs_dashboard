fc651be3cb57eff4bf4fc8ec07e864766763844d
Bessala <Bessalaabitang@gmail.com>
2025-12-18 16:41:35 +0100

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana, Geist  } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700']})
+export const geist = Geist({ subsets: ['latin'] });
